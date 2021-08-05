3059245599ee20a20beede1d3c1d73f984b11160
Federico Grandi <fgrandi30@gmail.com>
2021-08-06 01:28:37 +0200

    chore: update examples

--- src/commands/sub.ts
+++ src/commands/sub.ts
@@ -5,6 +5,7 @@ export const command: CommandOptions = {
   name: 'sub',
   description: 'Subcommands test.',
   guildID: testGuildID,
+
   options: [
     {
       name: 'group',
@@ -14,6 +15,23 @@ export const command: CommandOptions = {
         { name: '1', description: 'one', type: 'SUB_COMMAND' },
         { name: '2', description: 'two', type: 'SUB_COMMAND' }
       ]
+    },
+    {
+      name: '1',
+      description: 'one',
+      type: 'SUB_COMMAND',
+      options: [{ type: 'STRING', name: 'a', description: '.' }]
+    },
+    {
+      name: '2',
+      description: 'two',
+      type: 'SUB_COMMAND',
+      options: [{ type: 'STRING', name: 'a', description: '.' }]
     }
-  ]
+  ],
+  run(int) {
+    console.log(JSON.stringify(int.options.data))
+    console.log(int.options.getString('a'))
+    int.reply({ content: 'ok', ephemeral: true })
+  }
 }
